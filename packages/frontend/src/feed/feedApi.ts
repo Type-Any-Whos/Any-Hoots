168dd7dee0efc4bbe108e39de2c864ec308b6fa7
kylejb <47502769+kylejb@users.noreply.github.com>
2021-05-09 20:48:22 -0400

    FE: Renamed, reorg, and fixed imports

--- packages/frontend/src/feed/feedApi.ts
+++ packages/frontend/src/feed/feedApi.ts
@@ -1,11 +1,11 @@
-import { get, post } from "../util";
+import { get, post } from "../utils/fetchApi";
 
 export function getFeed(): Promise<Tweet[]> {
-  return get("api/posts");
+    return get("api/posts");
 }
 
 export function submitTweet(tweet: { text: string }): Promise<void> {
-  return post("api/posts", {
-    text: tweet.text,
-  });
+    return post("api/posts", {
+        text: tweet.text,
+    });
 }
