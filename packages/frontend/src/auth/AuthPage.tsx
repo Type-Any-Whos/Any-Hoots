fae1e254dec34a8dcb62d204645615f210df18e8
Matthew Leng <mleng89@gmail.com>
2021-05-12 13:26:20 -0400

    FE: Created new folder for png and svgs and implemented logo

--- packages/frontend/src/auth/AuthPage.tsx
+++ packages/frontend/src/auth/AuthPage.tsx
@@ -1,79 +1,85 @@
-import { Grid, Typography, FormControl } from "@material-ui/core";
-import { useContext, FormEvent } from "react";
-import { StateContext, ContextType } from "../StateProvider";
-import { Link, Redirect } from "react-router-dom";
-import Form, { FormInput } from "../components/form";
-import { handleAuthSubmit } from "../utils/formApi";
+import { Grid, Typography, FormControl } from '@material-ui/core';
+import { useContext, FormEvent } from 'react';
+import { StateContext, ContextType } from '../StateProvider';
+import { Link, Redirect } from 'react-router-dom';
+import Form, { FormInput } from '../components/form';
+import { handleAuthSubmit } from '../utils/formApi';
 
-import image from '../../../../undraw_Social_networking_re_i1ex-removebg-preview.png';
+import image from '../images/undraw_Social_networking_re_i1ex-removebg-preview.png';
 
 export default function AuthPage() {
-    const AuthFormFooter = window.location.pathname === "/auth/register"
-        ? <>Already have an account? <Link to="/auth/login">Sign in</Link>.</>
-        : <>Don't have an account? <Link to="/auth/register">Sign Up</Link>.</>;
+	const AuthFormFooter =
+		window.location.pathname === '/auth/register' ? (
+			<>
+				Already have an account? <Link to='/auth/login'>Sign in</Link>.
+			</>
+		) : (
+			<>
+				Don't have an account? <Link to='/auth/register'>Sign Up</Link>.
+			</>
+		);
 
-    const AuthFormSubmitBtnValue = window.location.pathname === "/auth/register"
-        ? "Register"
-        : "Login";
+	const AuthFormSubmitBtnValue =
+		window.location.pathname === '/auth/register' ? 'Register' : 'Login';
 
-    const { state, dispatch } = useContext<ContextType>(StateContext);
+	const { state, dispatch } = useContext<ContextType>(StateContext);
 
-    if (state.user) {
-        return <Redirect to="/" />;
-    }
+	if (state.user) {
+		return <Redirect to='/' />;
+	}
 
-    return (
-        <Grid container>
-            <Grid item xs={8}>
-                <Grid container>
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={10}>
-                        <Typography variant="h4">Welcome to AnyHoots!</Typography>
-                        <img src={image} />
-                        <Typography variant="h6">Make new friends!</Typography>
-                        <Typography variant="h6">Talk about things!</Typography>
-                        <Typography variant="h6">Be part of a community!</Typography>
-                    </Grid>
-                </Grid>
-            </Grid>
+	return (
+		<Grid container>
+			<Grid item xs={8}>
+				<Grid container>
+					<Grid item xs={2}></Grid>
+					<Grid item xs={10}>
+						<Typography variant='h4'>Welcome to AnyHoots!</Typography>
+						<img src={image} />
+						<Typography variant='h6'>Make new friends!</Typography>
+						<Typography variant='h6'>Talk about things!</Typography>
+						<Typography variant='h6'>Be part of a community!</Typography>
+					</Grid>
+				</Grid>
+			</Grid>
 
-            <Grid item xs={4}>
-                <Grid container>
-                    <Grid item xs={1}>
-                        {/* Empty grid for spacing */}
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Form onSubmit={(evt) => handleAuthSubmit(evt, dispatch)}>
-                            <FormControl fullWidth>
-                                <FormInput
-                                    id="handle"
-                                    name="handle"
-                                    type="text"
-                                    placeholder="Handle"
-                                />
-                            </FormControl>
+			<Grid item xs={4}>
+				<Grid container>
+					<Grid item xs={1}>
+						{/* Empty grid for spacing */}
+					</Grid>
+					<Grid item xs={4}>
+						<Form onSubmit={(evt) => handleAuthSubmit(evt, dispatch)}>
+							<FormControl fullWidth>
+								<FormInput
+									id='handle'
+									name='handle'
+									type='text'
+									placeholder='Handle'
+								/>
+							</FormControl>
 
-                            <FormControl fullWidth>
-                                <FormInput
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    placeholder="Password"
-                                />
-                            </FormControl>
+							<FormControl fullWidth>
+								<FormInput
+									id='password'
+									name='password'
+									type='password'
+									placeholder='Password'
+								/>
+							</FormControl>
 
-                            <FormControl fullWidth>
-                                <FormInput
-                                    name="submit-btn"
-                                    type="submit"
-                                    value={AuthFormSubmitBtnValue}
-                                />
-                            </FormControl>
-                        </Form>
-                        {AuthFormFooter}
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Grid>
-    );
+							<FormControl fullWidth>
+								<FormInput
+									name='submit-btn'
+									type='submit'
+									value={AuthFormSubmitBtnValue}
+								/>
+							</FormControl>
+						</Form>
+						{AuthFormFooter}
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 }
--- packages/frontend/src/layout/RightBar.tsx
+++ packages/frontend/src/layout/RightBar.tsx
@@ -1,15 +1,15 @@
-import Grid from "@material-ui/core/Grid";
+import Grid from '@material-ui/core/Grid';
 
-import image from '../../../../undraw_site_content_ihgn-removebg-preview.png';
-
+// import image from '../../../../';
+import image from '../images/undraw_site_content_ihgn-removebg-preview.png';
 export default function RightBar() {
-  return (
-    <Grid item xs={4}>
-      <img src={image} style={{position: 'fixed'}} />
-      <Grid container>
-        <Grid item xs={6}></Grid>
-        <Grid item xs={12} /> {/* Leave half the page */}
-      </Grid>
-    </Grid>
-  );
+	return (
+		<Grid item xs={4}>
+			<img src={image} style={{ position: 'fixed' }} />
+			<Grid container>
+				<Grid item xs={6}></Grid>
+				<Grid item xs={12} /> {/* Leave half the page */}
+			</Grid>
+		</Grid>
+	);
 }
